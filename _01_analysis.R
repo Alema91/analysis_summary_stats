76bfd2b2139105fe79f88705f70d0c905744ec50
Alema91 <alberto.lema.blanco@gmail.com>
2022-08-05 14:10:41 +0200

    Analysis of median and 10% coverage: 2; RSME

--- _01_analysis.R
+++ _01_analysis.R
@@ -10,6 +10,7 @@ library(ggplot2, quietly = TRUE, warn.conflicts = FALSE)
 library(forcats, quietly = TRUE, warn.conflicts = FALSE)
 library(reshape2, quietly = TRUE, warn.conflicts = FALSE)
 library(stringr, quietly = TRUE, warn.conflicts = FALSE)
+library(Metrics, quietly = TRUE, warn.conflicts = FALSE)
 
 ### Not in o in ----
 
@@ -40,3 +41,61 @@ mqc_filter <- data_summary_mqc[data_summary_mqc$Sample %in% id_luis, ]
 
 write.table(mapping_filter, "output/filter/mapping_illumina_filter.xlsx", sep = "\t", row.names = F, quote = F)
 write.table(mqc_filter, "output/filter/summary_mqc_filter.csv", sep = ",", row.names = F, quote = F)
+
+
+#### Analisis de la variacion en el coverage entre el original del multiqc y mi calculo de la coverage
+
+# Data multiqc
+data_mqc_multiqc_v2 <- read.csv2("data/mqc_multiqc/summary_variants_metrics_mqc_v2.csv", sep = ",")
+data_mqc_multiqc_v4 <- read.csv2("data/mqc_multiqc/summary_variants_metrics_mqc_v4.csv", sep = ",")
+
+# modificar tablas
+data_mqc_multiqc <- rbind(data_mqc_multiqc_v2, data_mqc_multiqc_v4)
+data_mqc_multiqc$Sample <- gsub("_", "-", data_mqc_multiqc$Sample)
+filter_multiqc_coverage <- data_mqc_multiqc[, c(1, 8, 10)]
+colnames(filter_multiqc_coverage) <- c("sample", "median_multiqc", "%10_multiqc")
+
+data_coverage <- read.csv2("data/df_coverage.txt", sep = "\t")
+filter_data_coverage <- data_coverage[data_coverage$sample %in% data_mqc_multiqc$Sample, ]
+colnames(filter_data_coverage) <- c("sample", "median_bu", "%10_bu")
+
+# diferencias
+samples_dif <- setdiff(filter_multiqc_coverage$sample, filter_data_coverage$sample)
+filter_2_multiqc_coverage <- filter_multiqc_coverage[filter_multiqc_coverage$sample %notin% samples_dif, ]
+
+# Data final
+df_coverage <- merge(filter_2_data_coverage, filter_2_multiqc_coverage)
+
+##### Analisis ----
+
+# df long
+df_coverage_long <- melt(df_coverage, id.var = c("sample"))
+df_coverage_long$value <- as.numeric(df_coverage_long$value)
+df_coverage_long$value[is.na(df_coverage_long$value)] <- 0
+
+# mediana
+df_median <- df_coverage_long[df_coverage_long$variable == c("median_bu", "median_multiqc"), ]
+df_median$variable <- factor(df_median$variable)
+
+# test wilconxon (Mann-Whitney test)
+wilcox.test(value ~ variable, data = df_median)
+
+# RSME
+rmse(as.numeric(df_coverage$"%10_bu"), as.numeric(df_coverage$"%10_multiqc"))
+
+# Plots
+ggplot(df_median, aes(value, color = variable)) +
+    geom_histogram(aes(y = ..density..), color = "black", fill = "white") +
+    geom_density(alpha = .2, fill = "#FF6666")
+
+# %10X
+df_10x <- df_coverage_long[df_coverage_long$variable == c("%10_bu", "%10_multiqc"), ]
+df_10x$variable <- factor(df_10x$variable)
+
+# test wilconxon (Mann-Whitney test)
+wilcox.test(value ~ variable, data = df_10x)
+
+# Plots
+ggplot(df_10x, aes(value, color = variable)) +
+    geom_histogram(aes(y = ..density..), color = "black", fill = "white") +
+    geom_density(alpha = .2, fill = "#FF6666")
